6b2863aba6ccdb2757337e7d44e11fa85c82f6ba
Ricardo SIGNES <rjbs@cpan.org>
2009-03-09 19:17:43 -0400

    first commit, fully working?

---
+++ lib/Email/MIME/Kit/KitReader/SWAK.pm
@@ -0,0 +1,58 @@
+package Email::MIME::Kit::KitReader::SWAK;
+use Moose;
+with 'Email::MIME::Kit::Role::KitReader';
+
+use Path::Resolver::Resolver::Mux::Prefix;
+use Path::Resolver::Resolver::FileSystem;
+use Path::Resolver::Resolver::AnyDist;
+
+has resolver => (
+  is       => 'ro',
+  does     => 'Path::Resolver::Role::Resolver',
+  init_arg => undef,
+  lazy     => 1,
+  default  => sub {
+    my ($self) = @_;
+    my $prs = sub { 'Path::Resolver::Resolver::' . $_[0] };
+
+    my $old_kr = $self->kit->kit_reader;
+    confess(__PACKAGE__ . ' must (for now) replace an existing KitReader::Dir')
+      unless $old_kr and $old_kr->isa('Email::MIME::Kit::KitReader::Dir');
+    
+    my $kit_resolver = $prs->('FileSystem')->new({
+      root => $self->kit->source,
+    });
+
+    Path::Resolver::Resolver::Mux::Prefix->new({
+      prefixes => {
+        fs   => $prs->('FileSystem')->new({ root => $self->fs_root }),
+        dist => $prs->('AnyDist')->new,
+        kit  => $kit_resolver,
+        q{}  => $kit_resolver,
+      },
+    });
+  },
+);
+
+has fs_root => (
+  is  => 'ro',
+  isa => 'Str',
+  default => '/',
+);
+
+sub get_kit_entry {
+  my ($self, $path) = @_;
+
+  my $content = $self->resolver->content_for($path);
+  return $content if $content;
+
+  confess "no content for $path";
+}
+
+sub BUILD {
+  my ($self) = @_;
+  $self->resolver;
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
